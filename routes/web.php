f40a596198adad3156ae2fb1b00909a3dd666ada
Herbich-dollar <nikodabotchonessa@gmail.com>
2026-01-27 10:59:59 +0000

    Mise à jour rapide

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,10 @@ Route::get('/', function () {
 Route::get('/projects', [ProjectController::class, 'index'])
     ->name('projects');
 
+// Détail d'un projet (accessible à tous)
+Route::get('/projects/{project}', [ProjectController::class, 'show'])
+    ->name('projects.show');
+
 // Pages statiques
 Route::get('/about', function () {
     return view('about');
@@ -56,53 +60,31 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
-/*
-|--------------------------------------------------------------------------
-| ADMIN (auth + admin)
-|--------------------------------------------------------------------------
-*/
-
-Route::middleware(['auth', 'admin'])->group(function () {
-
-    // Liste admin des projets
-    Route::get('/admin/projects', [ProjectController::class, 'adminIndex'])
-        ->name('projects.admin');
-
-    // Créer un projet
-    Route::get('/admin/projects/create', [ProjectController::class, 'create'])
+    // Gestion des projets (accessible à tous les utilisateurs connectés)
+    Route::get('/projects/create', [ProjectController::class, 'create'])
         ->name('projects.create');
 
-    Route::post('/admin/projects', [ProjectController::class, 'store'])
+    Route::post('/projects', [ProjectController::class, 'store'])
         ->name('projects.store');
 
-    // Modifier un projet
-    Route::get('/admin/projects/{project}/edit', [ProjectController::class, 'edit'])
+    Route::get('/projects/{project}/edit', [ProjectController::class, 'edit'])
         ->name('projects.edit');
 
-    Route::put('/admin/projects/{project}', [ProjectController::class, 'update'])
+    Route::put('/projects/{project}', [ProjectController::class, 'update'])
         ->name('projects.update');
 
-    // Supprimer un projet
-    Route::delete('/admin/projects/{project}', [ProjectController::class, 'destroy'])
+    Route::delete('/projects/{project}', [ProjectController::class, 'destroy'])
         ->name('projects.destroy');
 
-});
-
-Route::get('/admin/projects/{project}', [ProjectController::class, 'show'])
-        ->name('projects.show');
-
-
-Route::middleware('auth')->group(function () {
+    // Investissement dans les projets
     Route::get('/projects/{project}/invest', [DonationController::class, 'create'])
         ->name('projects.invest');
 
     Route::post('/projects/{project}/invest', [DonationController::class, 'store'])
         ->name('projects.invest.store');
-});
 
-Route::middleware('auth')->group(function () {
+    // Paiement Stripe
     Route::post('/projects/{project}/pay', [StripePaymentController::class, 'checkout'])
         ->name('stripe.checkout');
 
@@ -113,7 +95,19 @@ Route::middleware('auth')->group(function () {
         ->name('stripe.cancel');
 });
 
+/*
+|--------------------------------------------------------------------------
+| ADMIN (auth + admin)
+|--------------------------------------------------------------------------
+*/
+
+Route::middleware(['auth', 'admin'])->group(function () {
+
+    // Liste admin des projets (si vous avez besoin d'une vue spéciale admin)
+    Route::get('/admin/projects', [ProjectController::class, 'adminIndex'])
+        ->name('projects.admin');
+
+});
+
+// Webhook Stripe (sans authentification)
 Route::post('/stripe/webhook', [StripeWebhookController::class, 'handle']);
-
-
-
